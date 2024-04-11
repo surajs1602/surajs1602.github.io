a408e83564f4c7b9b62d9033454d01c259ac411d
surajs1602 <85102055+surajs1602@users.noreply.github.com>
2024-04-11 18:32:41 +0000

    Portfolio Modifications

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontent = document.getElementsByClassName("tab-contents");
+var worklist = document.getElementsByClassName("work-list");
+var workblocks = document.getElementsByClassName("work-blocks");
 
 function opentab(tabname){
     for(tabl of tablinks){
@@ -10,4 +12,17 @@ function opentab(tabname){
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openBlock(tabname){
+    document.getElementById("grid-block").classList.add("disable");
+    document.getElementById(tabname).classList.add("active-block");
+}
+
+function closeblock(){
+    for(works of workblocks){
+        works.classList.remove("active-block");
+    }
+    document.getElementById("grid-block").classList.remove("disable");
+
 }
